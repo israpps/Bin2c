45218df901b7af7a0dfc1144a87077474224b716
El_isra <tatochin-m@hotmail.com>
2022-03-28 11:18:15 -0300

    general cleanup

--- bin2c.c
+++ bin2c.c
@@ -1,4 +1,4 @@
-/*
+/**
  * This is bin2c program, which allows you to convert binary file to
  * C language array, for use as embedded resource, for instance you can
  * embed graphics or audio file directly into your program.
@@ -14,10 +14,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifdef USE_BZ2
-#include <bzlib.h>
-#endif
-
 int
 main(int argc, char *argv[])
 {
@@ -26,11 +22,6 @@ main(int argc, char *argv[])
     unsigned int i, file_size, need_comma;
 
     FILE *f_input, *f_output;
-
-#ifdef USE_BZ2
-    char *bz2_buf;
-    unsigned int uncompressed_size, bz2_size;
-#endif
 
     if (argc < 4) {
         fprintf(stderr, "Usage: %s binary_file output_file array_name\n",
@@ -55,30 +46,6 @@ main(int argc, char *argv[])
     fread(buf, file_size, 1, f_input);
     fclose(f_input);
 
-#ifdef USE_BZ2
-    // allocate for bz2.
-    bz2_size =
-      (file_size + file_size / 100 + 1) + 600; // as per the documentation
-
-    bz2_buf = (char *) malloc(bz2_size);
-    assert(bz2_buf);
-
-    // compress the data
-    int status =
-      BZ2_bzBuffToBuffCompress(bz2_buf, &bz2_size, buf, file_size, 9, 1, 0);
-
-    if (status != BZ_OK) {
-        fprintf(stderr, "Failed to compress data: error %i\n", status);
-        return -1;
-    }
-
-    // and be very lazy
-    free(buf);
-    uncompressed_size = file_size;
-    file_size = bz2_size;
-    buf = bz2_buf;
-#endif
-
     f_output = fopen(argv[2], "w");
     if (f_output == NULL) {
         fprintf(stderr, "%s: can't open %s for writing\n", argv[0], argv[2]);
@@ -100,14 +67,7 @@ main(int argc, char *argv[])
         fprintf(f_output, "0x%.2x", buf[i] & 0xff);
     }
     fprintf(f_output, "\n};\n\n");
-
     fprintf(f_output, "const int %s_length = %i;\n", ident, file_size);
-
-#ifdef USE_BZ2
-    fprintf(f_output, "const int %s_length_uncompressed = %i;\n", ident,
-            uncompressed_size);
-#endif
-
     fclose(f_output);
 
     return 0;
